5e2bfa9ba7bdfbbecfc076b35fc5e9c9e1614281
tomimandalap <tommymandalaputra@gmail.com>
2020-12-28 16:22:05 +0700

    progress 40

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $.getJSON('/data/foodanddrink.json', function(response) {
     $('#menu-list').append(`
       <div class="col-6 col-md-4">
         <div class="card mb-3">
-          <img src="img/`+ data.image +`" class="card-img-top" alt="`+ data.category +`">
+          <img src="img/`+ data.image +`" class="card-img-top" alt="`+ data.category +`" id="img-cursor">
           <div class="card-body">
             <h5 class="card-title">`+ data.name +`</h5>
             <span class="prices">Rp. `+ data.price +`</span>
@@ -16,11 +16,22 @@ $.getJSON('/data/foodanddrink.json', function(response) {
   })
 })
 
-// var 	bilangan = 10000;
-		
-// var	reverse = bilangan.toString().split('').reverse().join(''),
-// 	ribuan 	= reverse.match(/\d{1,3}/g);
-// 	ribuan	= ribuan.join('.').split('').reverse().join('');
+// klik button
+$(document).ready(function() {
+  $("#sidebarCollapse").on('click',function() {
+    $("#side-left").toggleClass('active')
+    $("#main").toggleClass('setting')
+  })
+})
 
-// // Cetak hasil	
-// console.log(ribuan)
+$(document).ready(function() {
+  $("#cart-btn").on('click',function() {
+    $("#side-right").toggleClass('active')
+  })
+})
+
+$(document).ready(function() {
+  $('#add-items').on('click', function() {
+    
+  })
+})
